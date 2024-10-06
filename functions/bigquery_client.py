cccc7a5fd118dfc4ca95ca5ed00d3a41ef5c1920
Niko <84403080+artadini@users.noreply.github.com>
2024-10-06 19:39:29 +0800

    documentation

--- functions/bigquery_client.py
+++ functions/bigquery_client.py
@@ -6,8 +6,20 @@ logger = get_logger("bigquery-client")
 
 
 def bigquery_client():
+    """
+    Creates and returns a BigQuery client using a service account key file for authentication.
+
+    The function sets the environment variable `GOOGLE_APPLICATION_CREDENTIALS` to the path of the
+    service account key file and then creates a BigQuery client.
+
+    Returns:
+        google.cloud.bigquery.Client: An authenticated BigQuery client.
+
+    Raises:
+        google.auth.exceptions.DefaultCredentialsError: If the credentials are not found or invalid.
+    """
     # Path to the service account key file
-    key_path = "/Users/nikolas.artadi/Documents/personal/Project Earthquake/bigquery-project-earthquake-secrets.json"
+    key_path = "/Users/nikolas.artadi/Documents/personal/project-earthquake/bigquery-project-earthquake-secrets.json"
     logger.info("Create  BigQuery client.")
     # Set the environment variable for authentication
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
@@ -16,8 +28,3 @@ def bigquery_client():
     client = bigquery.Client()
 
     return client
-
-# Example usage
-if __name__ == "__main__":
-    client = bigquery_client()
-    logger.info("BigQuery client created successfully.")
